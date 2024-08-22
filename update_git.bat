9cab6846dc7a7e8f8c7d13b841d6d3d0e220a59e
BaoBiBo <ng.hoangbao03@gmail.com>
2024-08-22 22:12:22 +0700

    Daily Challenge

--- update_git.bat
+++ update_git.bat
@@ -3,11 +3,6 @@ git add .
 git commit -m "Daily Challenge"
 git push
 
-for /f "tokens=2 delims==" %%I in ('"prompt $E&for %%A in (C) do @echo prompt $E"') do set "ORIG_COLOR=%%I"
-
-color 0A
-echo Daily Challenge have been pushed!
-
-color %ORIG_COLOR%
+echo [102;30m"Daily Challenge have been pushed!"[0m
 
 pause
