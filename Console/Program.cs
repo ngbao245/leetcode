8dd82d16e0e16f2692a1c57c0039df23b1ed0c5b
BaoBiBo <ng.hoangbao03@gmail.com>
2025-07-15 16:10:18 +0700

    Add _3136_ValidWord class with IsValid method
    Introduced a new class `_3136_ValidWord` in `3136_ValidWord.cs` that includes the `IsValid` method for validating words based on vowel and consonant criteria. Updated `Program.cs` to demonstrate the usage of `IsValid` and commented out the previously active `GetDecimalValue` method and related logic.

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,54 @@
-﻿using System;
-using LeetCodeCS.MyToy;
+﻿using LeetCodeCS.MyToy;
+
+System.Console.WriteLine(IsValid("IS"));
+
+static bool IsValid(string word)
+{
+    int vowels = 0;
+    int count = 0;
+    bool res = true;
+    int consonant = 0;
+    foreach (char c in word)
+    {
+        if ((char)c.ToString().ToLower().First() == (char)c.ToString().ToLower().First())
+        {
+            count++;
+        }
+    }
+
+    foreach (char c in word)
+    {
+        if (!((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122)))
+        {
+            res = false;
+        }
+    }
+
+    foreach (char c in word)
+    {
+        if ((char)c.ToString().ToLower().First() == 'a' || (char)c.ToString().ToLower().First() == 'e' ||
+                        (char)c.ToString().ToLower().First() == 'i' || (char)c.ToString().ToLower().First() == 'o' ||
+                        (char)c.ToString().ToLower().First() == 'u')
+        {
+            vowels++;
+            if (count >= 3 && res && consonant > 0)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            if (!(c >= 48 && c <= 57)) consonant++;
+        }
+    }
+
+    if (consonant == 0) res = false;
+
+    if (count < 3 || vowels == 0) return false;
+    return res;
+}
+
+
 
 //ListNode head = new ListNode(1,
 //                    new ListNode(2,
@@ -15,34 +64,34 @@ using LeetCodeCS.MyToy;
 //                    )
 //                );
 
-int[] bits = { 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 };
-ListNode head = BuildList.Build(bits);
+//int[] bits = { 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 };
+//ListNode head = BuildList.Build(bits);
 
 
-System.Console.WriteLine(GetDecimalValue(head));
+//System.Console.WriteLine(GetDecimalValue(head));
 
-static int GetDecimalValue(ListNode head)
-{
-    int count = 0;
-    int result = 0;
-    string binaryString = string.Empty;
+//static int GetDecimalValue(ListNode head)
+//{
+//    int count = 0;
+//    int result = 0;
+//    string binaryString = string.Empty;
 
-    while (head != null)
-    {
-        binaryString += head.val.ToString();
-        count++;
-        head = head.next;
-    }
+//    while (head != null)
+//    {
+//        binaryString += head.val.ToString();
+//        count++;
+//        head = head.next;
+//    }
 
-    int temp = count - 1;
+//    int temp = count - 1;
 
-    for (int i = 0; i < count; i++)
-    {
-        if (binaryString[i] == '1')
-        {
-            result += (int)Math.Pow(2, temp);
-        }
-        temp--;
-    }
-    return result;
-}
+//    for (int i = 0; i < count; i++)
+//    {
+//        if (binaryString[i] == '1')
+//        {
+//            result += (int)Math.Pow(2, temp);
+//        }
+//        temp--;
+//    }
+//    return result;
+//}
---
+++ LeetCodeCS/3136_ValidWord.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeCS;
+
+public class _3136_ValidWord
+{
+    public class Solution
+    {
+        public bool IsValid(string word)
+        {
+            int vowels = 0;
+            int count = 0;
+            bool res = true;
+            int consonant = 0;
+            foreach (char c in word)
+            {
+                if ((char)c.ToString().ToLower().First() == (char)c.ToString().ToLower().First())
+                {
+                    count++;
+                }
+            }
+
+            foreach (char c in word)
+            {
+                if (!((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122)))
+                {
+                    res = false;
+                }
+            }
+
+            foreach (char c in word)
+            {
+                if ((char)c.ToString().ToLower().First() == 'a' || (char)c.ToString().ToLower().First() == 'e' ||
+                                (char)c.ToString().ToLower().First() == 'i' || (char)c.ToString().ToLower().First() == 'o' ||
+                                (char)c.ToString().ToLower().First() == 'u')
+                {
+                    vowels++;
+                    if (count >= 3 && res && consonant > 0)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (!(c >= 48 && c <= 57)) consonant++;
+                }
+            }
+
+            if (consonant == 0) res = false;
+
+            if (count < 3 || vowels == 0) return false;
+            return res;
+        }
+    }
+}
